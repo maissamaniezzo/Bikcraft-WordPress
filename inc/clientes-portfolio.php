93ed6f4aa133462f47da1110f3db98c14716a5b5
Maíssa Maniezzo <maissamaniezzo@gmail.com>
2021-04-14 13:33:23 -0300

    Advanced Custom Fields para Qualidade e Portfólio

--- inc/clientes-portfolio.php
+++ inc/clientes-portfolio.php
@@ -1,10 +1,16 @@
-                <ul class="portfolio_lista">
-                    <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retro"></li>
-                    <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></li>
-                    <li class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></li>
-                </ul>
-                <ul class="portfolio_lista">
-                    <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></li>
-                    <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retro"></li>
-                    <li class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></li>
-                </ul>
+<?php $portfolio = get_page_by_title('portfolio'); ?>
+
+<ul class="portfolio_lista">
+    <?php if(have_rows('item_portfolio', $portfolio)): while(have_rows('item_portfolio', $portfolio)) : the_row(); ?>
+        <li class="grid-8"><img src="<?php the_sub_field('portifolio_imagem1', $portfolio); ?>" alt="<?php the_sub_field('portifolio_imagem_descricao1', $portfolio); ?>"></li>
+        <li class="grid-8"><img src="<?php the_sub_field('portifolio_imagem2', $portfolio); ?>" alt="<?php the_sub_field('portifolio_imagem_descricao2', $portfolio); ?>"></li>
+        <li class="grid-16"><img src="<?php the_sub_field('portifolio_imagem3', $portfolio); ?>" alt="<?php the_sub_field('portifolio_imagem_descricao3', $portfolio); ?>"></li>
+    <?php endwhile; else : endif; ?>
+</ul>
+
+<?php if (!is_page('portfolio')) { ?>
+    <div class="call">
+        <p><?php the_field('chamada_portfolio', $portfolio); ?></p>
+        <a href="/bikcraft/portfolio/" class="btn">Portfólio</a>
+    </div>
+<?php } ?>
