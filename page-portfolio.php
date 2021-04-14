29f125d62623ee0823708dec96efaf5432414ace
Maíssa Maniezzo <maissamaniezzo@gmail.com>
2021-04-14 15:23:45 -0300

    Advanced Custom Fields Página Portfólio

--- page-portfolio.php
+++ page-portfolio.php
@@ -9,18 +9,12 @@
 
         <!-- <section class="container fadeInDown" data-slide="quote" data-anime="1200"> -->
         <section class="container fadeInDown">
-            <blockquote class="quote_cliente">
-                <p>"Pedalar sempre foi minha paixão, o que o pessoal da Bikcraft fez foi intensificar o meu amor por esta atividade. Recomendo à todos que amo."</p>
-                <cite>Barbara Moss</cite>
-            </blockquote>
-            <blockquote class="quote_cliente">
-                <p>"Nada melhor do que dar um rolê com a minha Bikcraft na orla. Essa é a minha companheira mais fiel, nunca me traiu e está sempre a minha disposição."</p>
-                <cite>Jhony Rato</cite>
-            </blockquote>
-            <blockquote class="quote_cliente">
-                <p>"Aqueles que ainda não possuem uma Bikcraft, não sabem o que estão perdendo. A precisão é absurda e a velocidade transcedental. Nunca vi nada igual."</p>
-                <cite>Bernardo </cite>
-            </blockquote>
+            <?php if(have_rows('quote_portfolio')): while(have_rows('quote_portfolio')) : the_row(); ?>
+                <blockquote class="quote_cliente">
+                    <?php the_sub_field('quote'); ?>
+                    <cite><?php the_sub_field('nome_quote'); ?></cite>
+                </blockquote>
+            <?php endwhile; else : endif; ?>
         </section>
 
         <section class="portfolio" >
