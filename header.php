425557691d8cc036d18b19cb023dd6677d4d2b45
Maíssa Maniezzo <maissamaniezzo@gmail.com>
2021-04-19 13:30:06 -0300

    Adicionando SEO

--- header.php
+++ header.php
@@ -4,18 +4,18 @@
 
     <head>
         <meta charset="utf-8">
-        <title><?php bloginfo('name'); ?></title>
-        <meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retro e Esporte.">
+        <title><?php bloginfo('name'); ?>  <?php wp_title('-'); ?> <?php the_field('title_seo'); ?></title>
+        <meta name="description" content="<?php bloginfo('name'); ?>  <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>">
 
         <meta property="og:type" content="website"/>
-        <meta property="og:title" content="<?php bloginfo('name'); ?>"/>
+        <meta property="og:title" content="<?php bloginfo('name'); ?>  <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
         <meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retro e Esporte."/>
-        <meta property="og:url" content="http://bikcraft.com"/>
-        <meta property="og:image" content="https://bikcraft.com/img/og-image.png"/>
+        <meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.png"/>
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
         <script>document.documentElement.classList.add("js");</script>
